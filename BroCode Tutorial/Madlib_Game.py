98c10142f499308ae4881d432ff2dc19f18b0f76
Luis Andrés Acuña Pérez <70827561+AndresAp01@users.noreply.github.com>
2025-01-29 17:40:50 -0600

    Added files

---
+++ BroCode Tutorial/Madlib_Game.py
@@ -0,0 +1,15 @@
+# Madlib game
+# it is a word game where you create a story
+# by filling in blanks with random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place or thing): ")
+adjective2 = input("Enter the second adjective: ")
+verb1 = input("Enter a verb ending with '-ing': ")
+adjective3 = input("Enter the final adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
+
