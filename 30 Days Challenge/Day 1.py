f1e086d1ea704b44402d7fbf99bbb7369ae6fb8c
Luis Andrés Acuña Pérez <andres.acunaperez@protonmail.com>
2025-01-29 20:03:44 -0600

    added 'Day 1' code Successfully completed day 1 of the challenge;)

--- 30 Days Challenge/Day 1.py
+++ 30 Days Challenge/Day 1.py
@@ -1,1 +1,55 @@
 #temp
+import math
+
+#1. Check the python version you are using
+#2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+print(3 + 4)
+print(3 - 4)
+print(3*4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("Andres")
+print("Acuna Perez")
+print("Costa Rica")
+print("I hope to enjoy 30 Days of Python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 + 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type({'name':'Andres'}))
+print(type({1, 2, 3}))
+print(type((1, 2, 3)))
+
+print("Exercise Level 3: Part 1")
+#Execise: Level 2 Already done
+#Exercise: Level 3
+
+#Number
+print(type(22))
+#String
+print(type("Andresssss"))
+#Boolean
+print(type(True))
+#List
+print(type([0, 1, 2, 3, 4]))
+#Tuple
+print(type(('earth', 'moon', 'venus', 'mercury')))
+#Set
+print(type({1, 22, 44, 56}))
+#Dict
+print(type({'first_name':'Andres',
+           'last_name':'Perez',
+           'age':22
+           }))
+
+print("Exercise Level 3: Part 2")
+#Given two points on a graph: (2, 3) & (10, 8), find the euclidean distance between them.
+#formula: d(p,q) = sqrt((p1 - q1)^2 + (p2 - q2)^2)
+print(math.sqrt((2 - 10) ** 2 + (3 - 8) ** 2))
+
+
+
