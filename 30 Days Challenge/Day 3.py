a4c37c106f1d058d410e4ddfbc07317d1f0ca9ec
Luis Andrés Acuña Pérez <andres.acunaperez@protonmail.com>
2025-02-01 19:15:12 -0600

    added files and updates

--- 30 Days Challenge/Day 3.py
+++ 30 Days Challenge/Day 3.py
@@ -25,7 +25,7 @@ a = 3
 b = 2
 
 #Arithmetic operations and assigning the result to a variable
-total = a + bd
+total = a + b
 diff = a - b
 product = a * b
 division = a / b
@@ -42,3 +42,71 @@ print('a / b = ', division)
 print('a % b = ', remainder)
 print('a // b = ', floor_division)
 print('a ** b = ', exponential)
+
+num_one = 3
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+
+print("total: ", total)
+print("difference: ", diff)
+print("product : ", product)
+print("division: ", division)
+print("remainder: ", remainder)
+
+radius = 10
+area_of_circle = 3.14 * radius ** 2
+print('Area of circle: ', area_of_circle)
+
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, "N")
+
+print(3 > 2)
+print(3 >= 2)    # True, because 3 is greater than 2
+print(3 < 2)     # False,  because 3 is greater than 2
+print(2 < 3)     # True, because 2 is less than 3
+print(2 <= 3)    # True, because 2 is less than 3
+print(3 == 2)    # False, because 3 is not equal to 2
+print(3 != 2)    # True, because 3 is not equal to 2
+print(len('mango') == len('avocado'))  # False
+print(len('mango') != len('avocado'))  # True
+print(len('mango') < len('avocado'))   # True
+print(len('milk') != len('meat'))      # False
+print(len('milk') == len('meat'))      # True
+print(len('tomato') == len('potato'))  # True
+print(len('python') > len('dragon'))   # False
+
+
+# Boolean comparison
+print('True == True: ', True == True)
+print('True == False: ', True == False)
+print('False == False:', False == False)
+print('True and True: ', True and True)
+print('True or False:', True or False)
+
+# Another way comparison
+print('1 is 1', 1 is 1)                   # True - because the data values are the same
+print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
+print('A in Andres', 'A' in 'Andres') # True - A found in the string
+print('B in Andres', 'B' in 'Andres') # False -there is no uppercase B
+print('coding' in 'coding for all') # True - because coding for all has the word coding
+print('a in an:', 'a' in 'an')      # True
+print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+
+print(2<3<4)# True - because both statements are true mas simplificado
+print(3 > 2 and 4 < 3) # False - because the second statement is false
+print(3 < 2 and 4 < 3) # False - because both statements are false
+print(3 > 2 or 4 > 3)  # True - because both statements are true
+print(3 > 2 or 4 < 3)  # True - because one of the statement is true
+print(3 < 2 or 4 < 3)  # False - because both statements are false
+print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
+print(not True)      # False - Negation, the not operator turns true to false
+print(not False)     # True
+print(not not True)  # True
+print(not not False) # False
